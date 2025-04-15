faca3e7bade44eec32d191dfe1038a8848ef6975
saritasubhra <saritasubhra1@gmail.com>
2025-04-15 22:17:48 +0530

    firebase.json changes 3

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 //MIDDLEWARES
 app.use(
   cors({
-    origin: ["https://app.explified.com/"],
+    origin: ["https://explified-home.web.app/", "http://localhost:5173"],
     credentials: true,
   })
 );
