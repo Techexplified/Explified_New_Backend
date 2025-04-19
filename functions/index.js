01ece798ee716ed03fd940a15262ca45049a884d
gaurav <connect.gauravkaushik@gmail.com>
2025-04-19 18:33:28 +0530

    adding one more route in the cors function

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://explified-home.web.app",
+    origin: ["https://explified-home.web.app" , "https://app.explified.com"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
   })
