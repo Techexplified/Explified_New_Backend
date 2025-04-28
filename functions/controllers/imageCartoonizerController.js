3607046928682fa936c198135050f6fb7b70b46b
saritasubhra <saritasubhra1@gmail.com>
2025-04-28 22:28:29 +0530

    cartoonizer api changes

--- functions/controllers/imageCartoonizerController.js
+++ functions/controllers/imageCartoonizerController.js
@@ -76,6 +76,6 @@ exports.createCartoon = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message, error: err });
   }
 };
