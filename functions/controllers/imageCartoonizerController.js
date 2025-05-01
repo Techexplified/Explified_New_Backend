ff1b96545133e29304bf0d0c11beffe91af0153b
saritasubhra <saritasubhra1@gmail.com>
2025-05-01 19:32:00 +0530

    multer check

--- functions/controllers/imageCartoonizerController.js
+++ functions/controllers/imageCartoonizerController.js
@@ -10,37 +10,37 @@ exports.createCartoon = async (req, res) => {
   try {
     const { type } = req.body;
     const file = req.file;
-    console.log(file);
-    if (!file || !type) {
-      return res.status(400).json({ message: "image and type are required" });
-    }
-    const data = new FormData();
-    data.append("image", fs.createReadStream(file.path));
-    data.append("type", type);
-    const options = {
-      origin: "https://explified-home.web.app",
-      method: "POST",
-      url: "https://cartoon-yourself.p.rapidapi.com/facebody/api/portrait-animation/portrait-animation",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "x-rapidapi-key": process.env.RAPID_API_KEY,
-        "x-rapidapi-host": "cartoon-yourself.p.rapidapi.com",
-        ...data.getHeaders(),
-      },
-      data: data,
-    };
-    const response = await axios.request(options);
-    // console.log(response.data);
-    const cartoonImage = response?.data?.data?.image_url || "";
-    fs.unlink(file.path, (err) => {
-      if (err) console.error("Failed to delete temp file:", err);
-      else console.log("Temp file deleted:", file.path);
-    });
+
+    // if (!file || !type) {
+    //   return res.status(400).json({ message: "image and type are required" });
+    // }
+    // const data = new FormData();
+    // data.append("image", fs.createReadStream(file.path));
+    // data.append("type", type);
+    // const options = {
+    //   origin: "https://explified-home.web.app",
+    //   method: "POST",
+    //   url: "https://cartoon-yourself.p.rapidapi.com/facebody/api/portrait-animation/portrait-animation",
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "x-rapidapi-key": process.env.RAPID_API_KEY,
+    //     "x-rapidapi-host": "cartoon-yourself.p.rapidapi.com",
+    //     ...data.getHeaders(),
+    //   },
+    //   data: data,
+    // };
+    // const response = await axios.request(options);
+    // // console.log(response.data);
+    // const cartoonImage = response?.data?.data?.image_url || "";
+    // fs.unlink(file.path, (err) => {
+    //   if (err) console.error("Failed to delete temp file:", err);
+    //   else console.log("Temp file deleted:", file.path);
+    // });
     // const newEntry = new ImageCartoonizer({ image, cartoonImage });
     // const docRef = await collection.add({ ...newEntry });
     res.status(201).json({
       message: "Image cartoonized successfully",
-      data: cartoonImage,
+      data: { file, type },
       // id: docRef.id,
       // ...newEntry,
     });
