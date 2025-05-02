1e5e338518583d46975ae415f12924bf457dd2c9
saritasubhra <saritasubhra1@gmail.com>
2025-05-02 19:04:08 +0530

    url encoded data 2

--- functions/controllers/imageCartoonizerController.js
+++ functions/controllers/imageCartoonizerController.js
@@ -2,35 +2,30 @@ const axios = require("axios");
 
 exports.createCartoon = async (req, res) => {
   try {
-    const { type } = req.body;
-    const file = req.file;
+    const { image, type } = req.body;
 
-    if (!file || !type) {
-      return res.status(400).json({ message: "Image and type are required" });
+    if (!image || !type) {
+      return res
+        .status(400)
+        .json({ message: "base64Image and type are required" });
     }
 
-    console.log("Received file buffer size:", file.buffer?.length);
-
-    // Convert image buffer to base64 string
-    const base64Image = file.buffer.toString("base64");
-
-    // Prepare x-www-form-urlencoded body
     const encodedParams = new URLSearchParams();
-    encodedParams.append("image", base64Image);
+    encodedParams.append("image", image);
     encodedParams.append("type", type);
 
-    const response = await axios.post(
-      "https://cartoon-yourself.p.rapidapi.com/facebody/api/portrait-animation/portrait-animation",
-      encodedParams,
-      {
-        headers: {
-          "x-rapidapi-key": process.env.RAPID_API_KEY,
-          "x-rapidapi-host": "cartoon-yourself.p.rapidapi.com",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
+    const options = {
+      method: "POST",
+      url: "https://cartoon-yourself.p.rapidapi.com/facebody/api/portrait-animation/portrait-animation",
+      headers: {
+        "x-rapidapi-key": process.env.RAPID_API_KEY,
+        "x-rapidapi-host": "cartoon-yourself.p.rapidapi.com",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: encodedParams,
+    };
 
+    const response = await axios.request(options);
     const cartoonImage = response?.data?.data?.image_url;
 
     res.status(201).json({
--- functions/routes/imageCartoonizerRoutes.js
+++ functions/routes/imageCartoonizerRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const multer = require("multer");
 const { createCartoon } = require("../controllers/imageCartoonizerController");
 
 const router = express.Router();
-const upload = multer(); // No disk storage, uses memory
 
-router.post("/cartoonize", upload.single("image"), createCartoon);
+router.post("/cartoonize", createCartoon);
 
 module.exports = router;
