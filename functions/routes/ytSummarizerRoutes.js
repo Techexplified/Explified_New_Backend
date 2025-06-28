205347e3eba152b820c603fae155975037115914
saritasubhra <saritasubhra1@gmail.com>
2025-06-28 18:58:58 +0530

    transllation feature in summarizer

--- functions/routes/ytSummarizerRoutes.js
+++ functions/routes/ytSummarizerRoutes.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const {
   youtubeSummary,
+  answerTranscript,
   youtubeTranscript,
+  languageChange,
 } = require("../controllers/ytSummarizerController");
 
 const router = express.Router();
 
 router.post("/transcript", youtubeTranscript);
+router.post("/translate-transcript", languageChange);
+router.post("/answerTranscript", answerTranscript);
 router.post("/summary", youtubeSummary);
 
 module.exports = router;
