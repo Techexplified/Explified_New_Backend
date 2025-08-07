6a4f6ac1e88e06b52ba87b72e0cb5354207cd6d3
saritasubhra <saritasubhra1@gmail.com>
2025-08-07 21:55:38 +0530

    corrections

--- functions/routes/geminiRoutes.js
+++ functions/routes/geminiRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { contentForPresentation, imageForPresentation } = require("../controllers/geminiController");
+const {
+  contentForPresentation,
+  imageForPresentation,
+} = require("../controllers/geminiController");
 const router = express.Router();
-
 
 // POST /api/gemini/topic
 router.post("/topic", contentForPresentation);
-router.post("/image",imageForPresentation);
+router.post("/image", imageForPresentation);
 
 module.exports = router;
